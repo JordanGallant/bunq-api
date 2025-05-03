eaf3772ec31d4e03ca1238084974df86f947ef2d
Jordan <39-12602@sae.edu.za>
2025-05-03 11:07:01 +0200

    change specs

--- main.py
+++ main.py
@@ -175,9 +175,9 @@ async def face_swap(
         except (ValueError, TypeError):
             raise HTTPException(status_code=500, detail="Invalid balance value")
 
-        if balance_value > 1000:
+        if balance_value > 25000:
             base_image_url = "https://i.ibb.co/tM6X9T81/8640671.jpg"
-        elif balance_value < 100:
+        elif balance_value < 24000 :
             base_image_url = "https://i.ibb.co/s938KWxH/angry-old-man-shouting.jpg"
         else:
             base_image_url = "https://default-image-url.com/image.jpg"  # Optional middle case
